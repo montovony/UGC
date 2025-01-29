5987f56e17cd6c20d6b2fddd4b06bbc7c6d800a9
montovony <montovony@gmail.com>
2025-01-28 23:02:45 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  // Railway configura automaticamente a variável PORT
 
 // Middleware para CORS
 app.use((req, res, next) => {
